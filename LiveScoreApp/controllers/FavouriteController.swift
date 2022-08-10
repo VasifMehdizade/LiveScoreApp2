ad8708ad21a47a7443079aecb9274e7b0b558fca
Vasif1997 <vasif.mehdi97@gmail.com>
2022-08-10 11:24:08 +0400

    favourite controller created

---
+++ LiveScoreApp/controllers/FavouriteController.swift
@@ -0,0 +1,19 @@
+//
+//  FavouriteController.swift
+//  LiveScoreApp
+//
+//  Created by Vasif Mehdizade on 10.08.22.
+//
+
+import UIKit
+
+class FavouriteController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+
+
